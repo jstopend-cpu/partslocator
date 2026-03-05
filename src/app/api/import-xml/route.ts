e3f1d16bf9fbc82485fcc582722ef50d3644e728
IOANNIS STAMATELOS <jstopend@gmail.com>
2026-03-06 00:22:40 +0200

    Fix 500 error in xml import and add upsert logic

--- src/app/api/import-xml/route.ts
+++ src/app/api/import-xml/route.ts
@@ -1,15 +1,11 @@
 import { query } from "@/lib/db";
 
-export async function GET() {
+export async function POST(request: Request) {
 try {
-// @ts-ignore
-const products: any = await querySELECT id, name, ean, supplier, price, stock  FROM products  ORDER BY id DESC;;
+const { xmlText } = await request.json();
 
-} catch (error) {
-console.error("Error fetching products", error);
-return new Response(
-JSON.stringify({ error: "Internal server error" }),
-{ status: 500, headers: { "Content-Type": "application/json" } }
-);
+} catch (error: any) {
+console.error("Import Error:", error);
+return new Response(JSON.stringify({ error: error.message || "Internal Server Error" }), { status: 500 });
 }
 }
