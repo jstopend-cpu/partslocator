77084ad7d0ab3c710ef75e5d09a857cf3b0a5f94
IOANNIS STAMATELOS <jstopend@gmail.com>
2026-03-08 13:51:31 +0200

    Fix db.js syntax by using plain javascript

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global;
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export const query = async (strings: TemplateStringsArray, ...values: any[]) => {
+export const query = async (strings, ...values) => {
 const sql = strings.reduce((acc, str, i) => acc + str + (values[i] !== undefined ? $${i + 1} : ""), "");
 return prisma.$queryRawUnsafe(sql, ...values);
 };
