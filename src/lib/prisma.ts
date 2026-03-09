89e5f63aa98675d1836e205450e0e158f2563d4a
IOANNIS STAMATELOS <jstopend@gmail.com>
2026-03-09 23:24:40 +0200

    Fix: Use datasourceUrl for Prisma initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,11 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
 return new PrismaClient({
-datasources: {
-db: {
-url: "postgresql://neondb_owner:npg_KNEMJt9qIZy0@ep-wild-darkness-ad7dmhn8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
-}
-}
+datasourceUrl: "postgresql://neondb_owner:npg_KNEMJt9qIZy0@ep-wild-darkness-ad7dmhn8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
 })
 }
 
