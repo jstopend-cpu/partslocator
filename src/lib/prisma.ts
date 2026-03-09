949ecce0c8c2e1143c514efe17f55c9ba738aff9
IOANNIS STAMATELOS <jstopend@gmail.com>
2026-03-09 23:23:04 +0200

    Hardcoded database URL to fix Vercel connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-return new PrismaClient()
+return new PrismaClient({
+datasources: {
+db: {
+url: "postgresql://neondb_owner:npg_KNEMJt9qIZy0@ep-wild-darkness-ad7dmhn8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
+}
+}
+})
 }
 
 declare global {
