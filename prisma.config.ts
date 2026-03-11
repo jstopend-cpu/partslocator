b21964068e4935dfd816a073abeb3bf677ea5c12
IOANNIS STAMATELOS <jstopend@gmail.com>
2026-03-11 20:41:08 +0200

    Fix: Align with Prisma 7 configuration rules

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
-datasource: {
-url: process.env.DATABASE_URL,
-},
+  datasource: {
+    url: "postgresql://neondb_owner:npg_KNEMJt9qIZy0@ep-wild-darkness-ad7dmhn8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require",
+  },
 })
