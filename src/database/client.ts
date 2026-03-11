449a3fac3ba6b5e9df8add40b0409c827e627d8a
IOANNIS STAMATELOS <jstopend@gmail.com>
2026-03-12 00:38:58 +0200

    Force region fra1 and fix npm dependencies

--- src/database/client.ts
+++ src/database/client.ts
@@ -20,9 +20,9 @@ export const getPrisma = () => {
     console.log('CLEAN_START_ID: A1B2C3D4')
     console.log('TIMESTAMP_CHECK:', Date.now())
     console.log('DB_CHECK: Attempting connection to Neon...')
-    const FINAL_DB_URL =
+    const NEON_URL =
       "postgresql://neondb_owner:npg_KNEMJt9qIZy0@ep-wild-darkness-ad7dmhn8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
-    const neonPool = new Pool({ connectionString: FINAL_DB_URL })
+    const neonPool = new Pool({ connectionString: NEON_URL })
     const adapter = new PrismaNeon(neonPool as any)
     globalThis.prisma = new PrismaClient({ adapter })
   }
