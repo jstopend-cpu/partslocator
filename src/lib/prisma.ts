267923d6e8ef697904574cd108bf456807acb20e
IOANNIS STAMATELOS <jstopend@gmail.com>
2026-03-11 23:53:13 +0200

    Hardcoded URL with Pool fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,9 +3,6 @@ import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
 import ws from 'ws'
 
-const NEON_URL =
-  "postgresql://neondb_owner:npg_KNEMJt9qIZy0@ep-wild-darkness-ad7dmhn8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
-
 if (typeof window === 'undefined') {
   neonConfig.webSocketConstructor = ws
 }
@@ -18,8 +15,9 @@ export const getPrisma = () => {
   if (typeof window !== 'undefined') return {} as any
 
   if (!globalThis.prisma) {
-    const connectionString = process.env.DATABASE_URL || NEON_URL
-    const neonPool = new Pool({ connectionString })
+    const directUrl =
+      "postgresql://neondb_owner:npg_KNEMJt9qIZy0@ep-wild-darkness-ad7dmhn8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
+    const neonPool = new Pool({ connectionString: directUrl })
     const adapter = new PrismaNeon(neonPool as any)
     globalThis.prisma = new PrismaClient({ adapter })
   }
